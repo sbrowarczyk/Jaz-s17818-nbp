40097e679f81b5a970ad21babb21652f63875237
Sebastian Browarczyk <sbrowarczyk@wp.pl>
2021-07-03 15:33:44 +0200

    fix typoo and change properties

--- jaz-s17818-nbp/src/main/java/pl/pjatk/jazs17818nbp/exceptionhandler/NBPExceptionHandler.java
+++ jaz-s17818-nbp/src/main/java/pl/pjatk/jazs17818nbp/exceptionhandler/NBPExceptionHandler.java
@@ -1,4 +1,4 @@
-package pl.pjatk.jazs17818nbp.eceptionhandler;
+package pl.pjatk.jazs17818nbp.exceptionhandler;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
